1da24dada1c709ba4405b3d225d2fdd8018d0ebc
Maurizio Conti <mconti65@gmail.com>
2019-01-06 18:33:29 +0100

    Aggiunto Gelati

---
+++ Gelati/Gelati/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gelati
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Gelato> gelati = new List<Gelato>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            StreamReader fin = new StreamReader("gelati.csv");
+            fin.ReadLine();
+
+            while (!fin.EndOfStream)
+            {
+                gelati.Add(new Gelato(fin.ReadLine()));
+            }
+
+            fin.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            lvGelati.ItemsSource = gelati;
+        }
+
+
+        private void VotaClick(object sender, RoutedEventArgs e)
+        {
+            Button b = sender as Button;
+            if (b != null)
+            {
+                Gelato g = b.Tag as Gelato;
+                if (g != null)
+                    g.Voti++;
+
+                lvGelati.Items.Refresh();
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            StreamWriter fout = new StreamWriter("gelati.csv");
+            fout.WriteLine("Nome;Prezzo;Porzione;Voti;imgUrl");
+
+            foreach (Gelato g in gelati)
+                fout.WriteLine($"{g.Nome};{g.Prezzo};{g.Porzione};{g.Voti};{g.imgUrl}");
+            fout.Close();
+        }
+    }
+}
---
+++ Gelati/Gelati/Models/Gelato.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Gelati
+{
+    class Gelato
+    {
+        public Gelato() { }
+        public Gelato(string riga)
+        {
+            string[] colonne = riga.Split(';');
+            Nome = colonne[0];
+
+            double prezzo;
+            double.TryParse(colonne[1], out prezzo);
+            Prezzo = prezzo;
+
+            int porzione;
+            Int32.TryParse(colonne[2], out porzione);
+            Porzione = porzione;
+
+            int voti;
+            Int32.TryParse(colonne[3], out voti);
+            Voti = voti;
+
+            imgUrl = colonne[4];
+        }
+
+        public string Nome { get; set; }
+        public double Prezzo { get; set; }
+        public string strPrezzo { get { return $"{Prezzo} euro/Kg."; } }
+        public int Porzione { get; set; }
+        public string strPorzione { get { return $"porzione da {Porzione} gr."; } }
+        public int Voti { get; set; }
+        public string strVoti { get { return $"{Voti} voti."; } }
+        public string imgUrl { get; set; }
+        public ImageSource Immagine { get { return new BitmapImage(new Uri(imgUrl, UriKind.Absolute)); } }
+
+    }
+}
